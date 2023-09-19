8ee38e1bd4482d9172e526b82eed10b129af82b0
Maple0922 <maple0922sept@gmail.com>
2023-09-19 21:23:09 +0900

    feat: メンションリストの追加、リファクタ

--- app/Http/Controllers/SDsController.php
+++ app/Http/Controllers/SDsController.php
@@ -4,10 +4,52 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
 use Carbon\Carbon;
+use Illuminate\Support\Str;
 
 class SDsController extends Controller
 {
     private array $slackChannelIds;
+
+    private const REPLACE_MENTION_USERNAME = [
+        "<@U03TB56BZ7H>" => "@jumpei ueda",
+        "<@U02G19EPNLB>" => "@ai kanayama",
+        "<@U038QAHQ9SR>" => "@kayoko ito",
+        "<@U02FF7RUSQ7>" => "@akiko nitta",
+        "<@U01U5Q2QQQ4>" => "@yui watanabe",
+        "<@U414GC4S3>" => "@shohei tanikawa",
+        "<@ULS98A2QZ>" => "@tomoyuki hada",
+        "<@U040SGWBQNL>" => "@ritsuko himoto",
+        "<@U040H7MRLE9>" => "@yuuka yamashita",
+        "<@U03UN3CHF7S>" => "@eri onoda",
+        "<@U40LXDRJT>" => "@kengo kitaku",
+        "<@U021PFA41EZ>" => "@futo nakajima",
+        "<@U01GYMNQ4E4>" => "@itsuki tabata",
+        "<@U037FRKMATV>" => "@nana imura",
+        "<@U04445TS1QQ>" => "@chiho suzuki",
+        "<@U05N2EYAVT5>" => "@haruka nishino",
+        "<@U02UZS2MCB1>" => "@hiroki nagai",
+        "<!subteam^S033HEA941W|@engineer_executive>" => "@engineer_executive",
+    ];
+
+    private const REPLACE_SEND_USERNAME = [
+        "U03TB56BZ7H" => "@jumpei ueda",
+        "U02G19EPNLB" => "@ai kanayama",
+        "U038QAHQ9SR" => "@kayoko ito",
+        "U02FF7RUSQ7" => "@akiko nitta",
+        "U01U5Q2QQQ4" => "@yui watanabe",
+        "U414GC4S3" => "@shohei tanikawa",
+        "ULS98A2QZ" => "@tomoyuki hada",
+        "U040SGWBQNL" => "@ritsuko himoto",
+        "U040H7MRLE9" => "@yuuka yamashita",
+        "U03UN3CHF7S" => "@eri onoda",
+        "U40LXDRJT" => "@kengo kitaku",
+        "U021PFA41EZ" => "@futo nakajima",
+        "U01GYMNQ4E4" => "@itsuki tabata",
+        "U037FRKMATV" => "@nana imura",
+        "U04445TS1QQ" => "@chiho suzuki",
+        "U05N2EYAVT5" => "@haruka nishino",
+        "U02UZS2MCB1" => "@hiroki nagai"
+    ];
 
     public function __construct()
     {
@@ -78,73 +120,46 @@ class SDsController extends Controller
         $body = $response->getBody();
         $messages = collect(json_decode($body)->messages);
 
-        $replaceMentionUsername = [
-            "<@U03TB56BZ7H>" => "@jumpei ueda",
-            "<@U02G19EPNLB>" => "@ai kanayama",
-            "<@U038QAHQ9SR>" => "@kayoko ito",
-            "<@U02FF7RUSQ7>" => "@akiko nitta",
-            "<@U01U5Q2QQQ4>" => "@yui watanabe",
-            "<@U414GC4S3>" => "@shohei tanikawa",
-            "<@ULS98A2QZ>" => "@tomoyuki hada",
-            "<@U040SGWBQNL>" => "@ritsuko himoto",
-            "<@U040H7MRLE9>" => "@yuuka yamashita",
-            "<@U03UN3CHF7S>" => "@eri onoda",
-            "<@U40LXDRJT>" => "@kengo kitaku",
-            "<@U021PFA41EZ>" => "@futo nakajima",
-            "<@U01GYMNQ4E4>" => "@itsuki tabata",
-            "<@U037FRKMATV>" => "@nana imura",
-            "<@U04445TS1QQ>" => "@chiho suzuki",
-            "<!subteam^S033HEA941W|@engineer_executive>" => "@engineer_executive",
-        ];
-
-        $replaceSendUsername = [
-            "U03TB56BZ7H" => "@jumpei ueda",
-            "U02G19EPNLB" => "@ai kanayama",
-            "U038QAHQ9SR" => "@kayoko ito",
-            "U02FF7RUSQ7" => "@akiko nitta",
-            "U01U5Q2QQQ4" => "@yui watanabe",
-            "U414GC4S3" => "@shohei tanikawa",
-            "ULS98A2QZ" => "@tomoyuki hada",
-            "U040SGWBQNL" => "@ritsuko himoto",
-            "U040H7MRLE9" => "@yuuka yamashita",
-            "U03UN3CHF7S" => "@eri onoda",
-            "U40LXDRJT" => "@kengo kitaku",
-            "U021PFA41EZ" => "@futo nakajima",
-            "U01GYMNQ4E4" => "@itsuki tabata",
-            "U037FRKMATV" => "@nana imura",
-            "U04445TS1QQ" => "@chiho suzuki"
-        ];
-
-        $replaceHTML = [
-            "@jumpei ueda" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@jumpei ueda</span>",
-            "@kengo kitaku" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@kengo kitaku</span>",
-            "@futo nakajima" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@futo nakajima</span>",
-            "@itsuki tabata" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@itsuki tabata</span>",
-            "@jumpei ueda" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@jumpei ueda</span>",
-            "@engineer_executive" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@engineer_executive</span>",
-            "@ai kanayama" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@ai kanayama</span>",
-            "@kayoko ito" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@kayoko ito</span>",
-            "@akiko nitta" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@akiko nitta</span>",
-            "@yui watanabe" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@yui watanabe</span>",
-            "@shohei tanikawa" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@shohei tanikawa</span>",
-            "@tomoyuki hada" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@tomoyuki hada</span>",
-            "@ritsuko himoto" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@ritsuko himoto</span>",
-            "@yuuka yamashita" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@yuuka yamashita</span>",
-            "@eri onoda" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@eri onoda</span>",
-            "@nana imura" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@nana imura</span>",
-            "@chiho suzuki" => "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>@chiho suzuki</span>",
-        ];
-
         return $messages
             ->filter(fn ($message) => isset($message->user))
-            ->map(fn ($message) => [
-                'user' => strtr(strtr($message->user, $replaceSendUsername), $replaceHTML),
-                'content' => strtr(strtr($message->text, $replaceMentionUsername), $replaceHTML),
-                'datetime' => Carbon::parse($message->ts)->timezone("Asia/Tokyo")->format('Y-m-d H:i:s'),
-                'date' => Carbon::parse($message->ts)->timezone("Asia/Tokyo")->format('Y-m-d'),
-                'time' => Carbon::parse($message->ts)->timezone("Asia/Tokyo")->format('H:i:s'),
-            ])
+            ->map(function ($message) {
+                $date = Carbon::parse($message->ts)->timezone("Asia/Tokyo")->format('Y-m-d');
+                $time = Carbon::parse($message->ts)->timezone("Asia/Tokyo")->format('H:i:s');
+                return [
+                    'user' => $this->addStyleToUsername(strtr($message->user, self::REPLACE_SEND_USERNAME)),
+                    'content' => $this->addStyleToContent(strtr($message->text, self::REPLACE_MENTION_USERNAME)),
+                    'datetime' => "{$date} {$time}",
+                    'date' => $date,
+                    'time' => $time
+                ];
+            })
             ->sortBy('datetime')
             ->values();
+    }
+
+    private function addStyleToUsername(string $text): string
+    {
+        return collect(self::REPLACE_SEND_USERNAME)
+            ->reduce(
+                fn ($text, $username) => Str::replace(
+                    $username,
+                    "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>{$username}</span>",
+                    $text
+                ),
+                $text
+            );
+    }
+
+    private function addStyleToContent(string $text): string
+    {
+        return collect(self::REPLACE_MENTION_USERNAME)
+            ->reduce(
+                fn ($text, $username) => Str::replace(
+                    $username,
+                    "<span class='px-1 font-weight-bold d-inline-block rounded' style='color: blue; background-color: skyblue'>{$username}</span>",
+                    $text
+                ),
+                $text
+            );
     }
 }
