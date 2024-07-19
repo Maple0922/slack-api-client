fec843b019406ae8b0d48c099358147bc59a1b96
Maple0922 <maple0922sept@gmail.com>
2024-07-19 11:04:35 +0900

    feat: 通知先チャンネルを変更

--- config/slack/webhook.php
+++ config/slack/webhook.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'engineerGeneral' => env('SLACK_ENGINEER_GENERAL_WEBHOOK_URL'),
+    'engineerDevPoint' => env('SLACK_ENGINEER_DEV_POINT_WEBHOOK_URL'),
     'notifyTest' => env('SLACK_NOTIFY_TEST_WEBHOOK_URL')
 ];
