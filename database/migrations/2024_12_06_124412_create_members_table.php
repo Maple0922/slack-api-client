7a2fbd068c2009d6724591b29f1ffde0e2cca895
Maple0922 <maple0922sept@gmail.com>
2024-12-09 13:26:11 +0900

    feat: メンバーテーブルを作成するmigrationファイル

---
+++ database/migrations/2024_12_06_124412_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->string('notion_id')->unique();
+            $table->string('name');
+            $table->string('image_url');
+            $table->integer('target_point');
+            $table->boolean('is_valid')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
